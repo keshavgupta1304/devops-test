a00adb27cf5de5fa3a5651673aea3da8406b9ac2
Keshav Gupta <117674355+keshavgupta1304@users.noreply.github.com>
2026-03-14 18:53:01 +0530

    Show user credentials in Home component
    Display user's username and password in the Home component.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,9 @@ export default async function Home() {
   const user = await prisma.user.findFirst()
   return (
     <div>
+      user:
       {user?.username}
+      password:
       {user?.password}
     </div>
   );
